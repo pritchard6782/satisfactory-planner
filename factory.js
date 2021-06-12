415b042cfabb9c0bbd453b0b0613d11954a9c406
pritchard6782 <pritchard6782@hotmail.com>
2021-06-12 11:28:00 +0100

    woo

--- factory.js
+++ factory.js
@@ -33,22 +33,16 @@ function createInputNode(text, iconUrl, canvas) {
         height: 100,
         width: 100,
         fill: '#eef',
-        originX: 'center',
-        originY: 'center'
+        border: '#333'
     });
 
-    var line = new fabric.Line({
-        x1: 100,
-        x2: 100,
-        y1: 150,
-        y2: 150,
-        fill: '#333'
+    var line = new fabric.Line([50, 100, 200, 100], {
+        stroke: '#333'
     });
     
     var text = new fabric.Text(text, {
         fontSize: 16,
-        originX: 'center',
-        originY: 'center'
+        originX: 'center'
     });
 
     fabric.Image.fromURL(iconUrl, icon => {
